fd4642d2ba31b9f1d189f89718ceef929cd28fe0
John Kryspin <johnkryspin@gmail.com>
2020-12-08 18:19:03 -0500

    index handles one and two parts cleanly

--- day08/index.js
+++ day08/index.js
@@ -1,43 +1,68 @@
 const { readFileByLine } = require("../helper")
 const array = readFileByLine("input", "08")
-console.log(array)
+function executeProgram(map) {
+    let opIndex = 0
+    let accumulator = 0
+    let stack = []
+    for (let i = 0; i < map.size; i++) {
+        const currOp = map.get(opIndex)
+        if (!currOp) {
+            return { stack, accumulator, infiniteLoop: false, }
+        }
+        const { op, visited, number } = currOp
+        stack.push(currOp)
+        if (visited) {
+            //stack
+            return { stack, accumulator, infiniteLoop: true, }
+        }
+        map.set(opIndex, { ...map.get(opIndex), visited: true })
+        if (op === "nop") {
+            opIndex++
+        }
+        if (op === "jmp") {
+            opIndex += number
+        }
+        if (op === "acc") {
+            opIndex++
+            accumulator += number
+        }
+    }
+}
+
 let map = new Map()
-let opIndex = 0
-let accumulator = 0
 for (let i = 0; i < array.length; i++) {
     const line = array[i]
-    map.set(i, { visited: false, op: line.substring(0, 3), relative: line.substring(4, 5) === "+", number: Number(line.match(/\d+/g)[0]) })
+    map.set(i, { visited: false, op: line.substring(0, 3), number: parseInt(line.substring(4)) })
 }
-let foundLoop = false
-while (!foundLoop) {
-    console.log({ opIndex })
-    const currOp = map.get(opIndex)
-    console.log({ currOp })
-    if (currOp.visited) {
-        foundLoop = true
-        console.log({ accumulator })
-        break;
-    }
-    map.set(opIndex, { ...map.get(opIndex), visited: true })
-    if (currOp.op === "nop") {
-        opIndex++
 
-    }
-    if (currOp.op === "jmp") {
-        if (currOp.relative) {
-            opIndex = opIndex + currOp.number
-        } else {
-            opIndex = opIndex - currOp.number
+
+function findIncorrectOp() {
+    for (let j = 0; j < array.length; j++) {
+        let map = new Map()
+        for (let i = 0; i < array.length; i++) {
+            const line = array[i]
+            const number = parseInt(line.substring(4))
+            const op = line.substring(0, 3)
+            if (j === i) {
+                if (op === "jmp")
+                    map.set(i, { visited: false, op: "nop", number })
+                else if (op === "nop")
+                    map.set(i, { visited: false, op: "jmp", number })
+                else
+                    map.set(i, { visited: false, op: op, number })
+
+            } else {
+                map.set(i, { visited: false, op: op, number })
+            }
         }
+        const answer = executeProgram(map)
+        if (answer.infiniteLoop === false)
+            return { answer, j }
     }
-    if (currOp.op === "acc") {
-        opIndex++
-        if (currOp.relative) {
-            accumulator += currOp.number
-        } else {
-            accumulator -= currOp.number
-        }
-    }
-    console.log("end op index:" + opIndex)
 }
-console.log({ accumulator })
+
+
+const part1 = executeProgram(map)
+console.log(part1)
+const part2 = findIncorrectOp()
+console.log(part2)
