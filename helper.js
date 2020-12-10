c93600f4881810cdf50d06e76b2646a53b471842
John Kryspin <johnkryspin@gmail.com>
2020-12-09 19:14:23 -0500

    this time with speed

--- helper.js
+++ helper.js
@@ -3,8 +3,11 @@ let path = require('path')
 var endOfLine = require('os').EOL;
 
 module.exports = {
-    readFileByLine(filename, day) {
+    readFileByLine(filename, day, map) {
         const file = fs.readFileSync(path.join(__dirname, "day" + day, filename))
-        return file.toString().split(endOfLine);
+        if (!map)
+            return file.toString().split(endOfLine)
+        return file.toString().split(endOfLine).map(map)
+
     }
 }
